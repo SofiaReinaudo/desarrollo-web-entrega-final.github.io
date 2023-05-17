75644091f5ec3b9a989ac875d9c7d1ed9aebafc2
Sofia Reinaudo <sofiareinaudo@gmail.com>
2023-05-16 21:05:51 -0300

    cambios js

--- js/contacto.js
+++ js/contacto.js
@@ -3,9 +3,12 @@ alert("Bienvenido a la sección de contacto. Aquí puedes escribir tus datos per
 edad = parseInt(prompt("Antes de continuar ingrese su edad. Se necesita ser mayor de 18 años para comprar."));
 
 if( edad >= 18){
-    alert("Sos mayor de 18 años! Podes continuar.")
+    alert("Sos mayor de 18 años! Puedes continuar.")
+}
+else if( edad < 18){
+    alert("Sos menor de 18 años, no puedes continuar.")
 }
 else{
-    alert("Sos menor de 18 años, no podes continuar.")
+    alert("Sólo se permiten números.")
 }
 
